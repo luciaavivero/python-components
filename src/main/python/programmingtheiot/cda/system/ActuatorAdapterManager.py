bf3abfdd5d53618bf20b82a3a8d1a52aebd6c9d8
luciaavivero <luciaavivero@gmail.com>
2025-03-12 21:02:36 +0100

    PIOT-CDA-04-004

--- src/main/python/programmingtheiot/cda/system/ActuatorAdapterManager.py
+++ src/main/python/programmingtheiot/cda/system/ActuatorAdapterManager.py
@@ -33,6 +33,20 @@ class ActuatorAdapterManager(object):
 
 			# create the HVAC actuator
 			self.hvacActuator = HvacActuatorSimTask()
+		else:
+			hueModule=import_module('programmingtheiot.cda.emulated.HumidifierEmulatorTask','HumidiferEmulatorTask')
+			hueClazz=getattr(hueModule,'HumidifierEmulatorTask')
+			self.humidifierActuator=hueClazz()
+
+			# create the HVAC actuator emulator
+			hveModule=import_module('programmingtheiot.cda.emulated.HvacEmulatorTask','HvacEmulatorTask')
+			hveClazz=getattr(hveModule,'HvacEmulatorTask')
+			self.hvacActuator=hveClazz()
+
+			# create the LED display actuator emulator
+			leDisplayModule=import_module('programmingtheiot.cda.emulated.LedDisplayEmulatorTask','LedDisplayEmulatorTask')
+			leClazz=getattr(leDisplayModule,'LedDisplayEmulatorTask')
+			self.ledDisplayActuator=leClazz()
 	
 	def __init__(self, dataMsgListener: IDataMessageListener = None):
 		self.dataMsgListener = dataMsgListener
