f75d01f78d2670394c3f464a1efb01b11b8250d2
luciaavivero <luciaavivero@gmail.com>
2025-05-27 12:39:45 +0200

    PIOT-CDA-10-001

--- src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
+++ src/main/python/programmingtheiot/cda/connection/MqttClientConnector.py
@@ -18,6 +18,8 @@ from programmingtheiot.common.ResourceNameEnum import ResourceNameEnum
 
 from programmingtheiot.cda.connection.IPubSubClient import IPubSubClient
 
+import ssl
+
 class MqttClientConnector(IPubSubClient):
 	"""
 	Shell representation of class for student implementation.
@@ -57,6 +59,14 @@ class MqttClientConnector(IPubSubClient):
 
 		self.mqttClient = None
 
+		self.enableEncryption = \
+			self.config.getBoolean( \
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.ENABLE_CRYPT_KEY)
+
+		self.pemFileName = \
+			self.config.getProperty( \
+				ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.CERT_FILE_KEY)
+
 		# IMPORTANT:
 		#
 		# You can choose to set clientID in a number of ways:
@@ -86,6 +96,18 @@ class MqttClientConnector(IPubSubClient):
 		if not self.mqttClient:
 			# TODO: make clean_session configurable
 			self.mqttClient = mqttClient.Client(client_id = self.clientID, clean_session = True)
+
+			try:
+				if self.enableEncryption:
+					logging.info("Enabling TLS encryption...")
+
+					self.port = \
+						self.config.getInteger( \
+							ConfigConst.MQTT_GATEWAY_SERVICE, ConfigConst.SECURE_PORT_KEY, ConfigConst.DEFAULT_MQTT_SECURE_PORT)
+
+					self.mqttClient.tls_set(self.pemFileName, tls_version = ssl.PROTOCOL_TLS_CLIENT)
+			except:
+				logging.warning("Failed to enable TLS encryption. Using unencrypted connection.")
 
 			self.mqttClient.on_connect = self.onConnect
 			self.mqttClient.on_disconnect = self.onDisconnect
