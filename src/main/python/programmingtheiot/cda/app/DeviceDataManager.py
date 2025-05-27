566500cf24c43c1a8204247b250cb75c5a21a866
luciaavivero <luciaavivero@gmail.com>
2025-05-27 14:02:53 +0200

    PIOT-CDA-10-004

--- src/main/python/programmingtheiot/cda/app/DeviceDataManager.py
+++ src/main/python/programmingtheiot/cda/app/DeviceDataManager.py
@@ -182,11 +182,21 @@ class DeviceDataManager(IDataMessageListener):
 		@return boolean
 		"""
 		if data:
-			logging.debug("Incoming sensor data received (from sensor manager): " + str(data))
+			logging.info("Incoming sensor data received (from sensor manager): " + str(data))
+
+			# TODO: Optionally, implement `_handleSensorDataAnalysis()` to handle internal analytics
 			self._handleSensorDataAnalysis(data)
+
+			# Convert the `SensorData` instance to JSON
+			jsonData = DataUtil().sensorDataToJson(data = data)
+
+			# Pass the resource and newly generated JSON data to `_handleUpstreamTransmission()`
+			self._handleUpstreamTransmission(resource = ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, msg = jsonData)
+
 			return True
 		else:
 			logging.warning("Incoming sensor data is invalid (null). Ignoring.")
+
 			return False
 	
 	def handleSystemPerformanceMessage(self, data: SystemPerformanceData = None) -> bool:
@@ -287,4 +297,18 @@ class DeviceDataManager(IDataMessageListener):
 		1) Check connection: Is there a client connection configured (and valid) to a remote MQTT or CoAP server?
 		2) Act on msg: If # 1 is true, send message upstream using one (or both) client connections.
 		"""
-		pass
+		logging.info("Upstream transmission invoked. Checking comm's integration.")
+
+		# NOTE: If using MQTT, the following will attempt to publish the message to the broker
+		if self.mqttClient:
+			if self.mqttClient.publishMessage(resource = resourceName, msg = msg):
+				logging.debug("Published incoming data to resource (MQTT): %s", str(resourceName))
+			else:
+				logging.warning("Failed to publish incoming data to resource (MQTT): %s", str(resourceName))
+
+		# NOTE: If using CoAP, the following will attempt to PUT the message to the server
+		if self.coapClient:
+			if self.coapClient.sendPutRequest(resource = resourceName, payload = msg):
+				logging.debug("Put incoming message data to resource (CoAP): %s", str(resourceName))
+			else:
+				logging.warning("Failed to put incoming message data to resource (CoAP): %s", str(resourceName))
